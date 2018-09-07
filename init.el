0bd27020cc2a86a444f6168e8dc37346b9614733
Joe DeCock <joseph.decock@thomsonreuters.com>
2018-09-07 16:31:06 -0500

    Organization and stuff
    Not really sure where I'm at anymore. Oh well.

--- init.el
+++ init.el
@@ -8,7 +8,13 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ivy-yasnippet zenburn-theme fill-column-indicator flx string-inflection yasnippet-snippets yasnippet evil-magit 4clojure counsel-projectile counsel helm melpa-upstream-visit projectile magit company tide)))
+    (use-package ivy-yasnippet zenburn-theme fill-column-indicator flx string-inflection yasnippet-snippets yasnippet evil-magit 4clojure counsel-projectile counsel helm melpa-upstream-visit projectile magit company tide)))
  '(show-paren-mode t))
 
 (load-file (concat user-emacs-directory "emacs.el"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
